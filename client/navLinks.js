3b872a51cb033f7368e811e219a50a8ed1f06feb
Santiago Villarroel <santiago.villarroel@fi.uncoma.edu.ar>
2025-03-05 00:01:38 -0300

    add extension to config import

--- client/navLinks.js
+++ client/navLinks.js
@@ -1,4 +1,4 @@
-import { URL_SERVER } from '../config';
+import { URL_SERVER } from '../config.js';
 
 document.getElementById("prolog-app-link").setAttribute('href', URL_SERVER)
 document.getElementById("about-link").setAttribute('href', URL_SERVER + 'about')
